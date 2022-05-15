fdc9673fa40c249950bda3e05c4041c2477f3556
Fernando Henrique <bxnandoxd@hotmail.com>
2022-05-15 14:50:05 -0300

    cria controller de criacao de usuario

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -7,10 +7,16 @@ interface IRequest {
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findByEmail(email);
+
+    if (user) throw new Error("User already exists");
+
+    const newUser = this.usersRepository.create({ email, name });
+
+    return newUser;
   }
 }
 
